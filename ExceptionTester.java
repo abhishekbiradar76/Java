642bc891c4691cbbe0693eb5f11698cab123d4a0
abhishekbiradar76 <89910683+abhishekbiradar76@users.noreply.github.com>
2021-12-06 11:28:19 -0500

    Add files via upload

---
+++ ExceptionTester.java
@@ -0,0 +1,14 @@
+package com.xworkz.exceptiontester;
+
+import java.util.HashSet;
+
+public class ExceptionTester {
+	
+		public static void main(String a[]){
+		String c=null;
+		String b= "BABA";
+		System.out.println(a.equals(b));
+		HashSet<E>
+		}
+		}
+
