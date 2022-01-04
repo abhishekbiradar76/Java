67998009d40bef779bc887baec2ee9552f0b8a12
abhishekbiradar76 <89910683+abhishekbiradar76@users.noreply.github.com>
2022-01-04 08:31:05 +0530

    Add files via upload

---
+++ train/Traa.java
@@ -0,0 +1,10 @@
+package com.train;
+
+public interface Traa {
+	String DRIVERCLASS="com.mysql.cj.jdbc.Driver";
+	String URL="jdbc:mysql://localhost:3306/train";
+	String USERNAME="root";
+	String PASSWORD="Shabita@123";
+
+}
+  
